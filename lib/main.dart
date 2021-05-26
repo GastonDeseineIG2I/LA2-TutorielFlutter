c15ca201ef0e503e431a930d4bce4ccd21d4f843
Gaston Deseine <gaston.deseine@ig2i.centralelille.fr>
2021-05-26 21:25:30 +0200

    Initiation aux notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:tutoriel_flutter/pages/login.page.dart';
 import 'package:tutoriel_flutter/services/locator.service.dart';
 
+/// Initialisation du plugin flutter pour les notifications
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
 void main() async {
+
+  await initNotification();
   await setupServiceLocator();
   runApp(MyApp());
+  _notify();
+}
+
+void _notify() async {
+  /// Paramètrage Android
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      'channel description',
+      icon: '@mipmap/ic_launcher',
+      largeIcon: DrawableResourceAndroidBitmap('@mipmap/ic_launcher'),
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'test_ticker'
+  );
+  /// Paramètrage IOS
+  var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+
+  var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+
+  await flutterLocalNotificationsPlugin.show(0, 'Bienvenue !', 'Voici votre premiere notification avec Flutter !!!',
+      platformChannelSpecifics);
+}
+
+Future initNotification() async {
+
+  /// Attend que le WidgetsBinding soit initialisé pour pouvoir paramètrer correctement les notifications
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Initialisation des paramètres Android
+  var initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  /// Note: Les permissions IOS eet Mac ne sont pas forcement requisent ici si elles sont déjà défini.
+  /// Initialisation des paramètres IOS
+  final IOSInitializationSettings initializationSettingsIOS =
+  IOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification:
+          (int id, String? title, String? body, String? payload) async {});
+  /// Initialisation des paramètres MacOS
+  const MacOSInitializationSettings initializationSettingsMacOS =
+  MacOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true);
+
+  /// Mappage des paramètres
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+      macOS: initializationSettingsMacOS);
+  /// Initialisation des paramètres
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String? payload) async {});
 }
 
 class MyApp extends StatelessWidget {
